90aa23e7c0e43fa53215d82d693d5581a64c331f
Jan Wijffels <jwijffels@bnosac.be>
2020-07-01 13:47:56 +0200

    Add simon2stage

--- R/pkg.R
+++ R/pkg.R
@@ -2,5 +2,8 @@
 #' @importFrom stats pbinom qbinom aggregate dbinom
 #' @importFrom data.table setDT rbindlist
 #' @importFrom Rcpp evalCpp
+#' @importFrom grDevices chull
+#' @importFrom graphics plot points lines
+#' @importFrom OneArmPhaseTwoStudy get_CI
 #' @useDynLib PhIIdesign
 NULL
