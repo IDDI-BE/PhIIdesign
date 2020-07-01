a4bd0a23ccbb37656566ed9980561a1286a36665
Jan Wijffels <jwijffels@bnosac.be>
2020-07-01 16:40:57 +0200

    Add example data for sargent2

---
+++ R/datasets.R
@@ -0,0 +1,9 @@
+#' @name data_sargent2
+#' @title Dataset with design settings for sargent2stage
+#' @description Dataset as displayed in the following paper:
+#' Sargent DJ, Goldberg RM. A Three-Outcome Design for Phase II Clinical Trials. Controlled Clinical Trials 22:117-125
+#' @docType data
+#' @examples
+#' data(data_sargent2)
+#' str(data_sargent2)
+NULL
