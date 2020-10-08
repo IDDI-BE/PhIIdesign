a996e3d96fc19b3e7e6d4638a95e7669bd016125
Jan Wijffels <jwijffels@bnosac.be>
2020-10-08 17:28:53 +0200

    Add rstudio addin Add app_start

---
+++ R/app.R
@@ -0,0 +1,19 @@
+
+#' @title Launch Shiny app for Phase II clinical trials
+#' @description Web application allowing to see designs.
+#' Note that this requires the rmarkdown, flexdashboard, shiny, shinyWidgets, ggplot2 and shinipsum packages to be installed
+#' @export
+#' @examples
+#' \dontrun{
+#' library(rmarkdown)
+#' library(flexdashboard)
+#' library(shiny)
+#' library(shinyWidgets)
+#' library(shinipsum)
+#' library(ggplot2)
+#' app_start()
+#' }
+app_start <- function(){
+  path <- system.file(package = "PhIIdesign", "dashboard", "ph2design.Rmd")
+  rmarkdown::run(path)
+}
